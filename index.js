ce93731b71c006bbe42687372d73e0488518138a
yamikuro365 <90952288+yamikuro365@users.noreply.github.com>
2024-04-04 18:10:34 +0700

    Update index.js

--- index.js
+++ index.js
@@ -73,8 +73,9 @@ function updateStatusAndSendMessages() {
   const nextStatus = statusMessages[(currentIndex + 1) % statusMessages.length];
 
   client.user.setPresence({
-    activities: [{ name: currentStatus, type: "PLAYING"}],
-    status: 'dnd',
+    activities: [{ name: currentStatus}],
+    type: 'PLAYING',
+    status: 'dnd'
   });
 
   
