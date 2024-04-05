db86690d481a686301c8f669726abaa2f3460554
yamikuro365 <90952288+yamikuro365@users.noreply.github.com>
2024-04-05 08:31:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["/play ลิงค์ หรือ ชื่อเพลง","Devby.YamikuroStudio!"];
+const statusMessages = ["/play ลิงค์ หรือ ชื่อเพลง","Devby.YamikuroStudio!","$serverCount"];
 
 
 let currentIndex = 0;
@@ -74,7 +74,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus}], //activities: [{ name: currentStatus}],
-    type: 'PLAYING',
+    type: 'STREAMING',
     status: 'invisible' // online, idle, invisible, dnd
   });
 
